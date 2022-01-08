dcc5634be6f05ebf8fba9700fa4aae3178b1ebd8
way99 <745855589@qq.com>
2022-01-08 11:01:42 +0800

    perf change 使用immutable.js来管理store中的数据

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,15 +1,20 @@
 import * as constants from './constants'
+import { fromJS } from 'immutable'
 
-const defaultState={
+// immutable库是facebook团队开发的,生成immutable对象
+// immutable  不可改变的
+
+const defaultState=fromJS({
     focused:false
-};
+});
 
 export default (state=defaultState,action)=>{
     switch(action.type){
         case constants.SEARCH_FOCUS:
-            return {focused:true}
+        // immutable对象的set方法，会结合之前immutable对象的值和设置的值返回一个全新的对象
+            return state.set("focused",true);
         case constants.SEARCH_BLUR:
-            return {focused:false}
+            return state.set("focused",false);
     }
     return state;
 }
