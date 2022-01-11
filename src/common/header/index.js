6647db30f442b079f73eca53351d2fb9c1f1f189
way99 <745855589@qq.com>
2022-01-11 16:22:59 +0800

    perf change 避免无意义的请求，提升组件性能

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -45,7 +45,7 @@ class Header extends Component {
     }
 
     render(){
-        const { focused,handleInputFocus,handleInputBlur }=this.props;
+        const { focused,handleInputFocus,handleInputBlur,list }=this.props;
         return (  
             <HeaderWrapper>
                 <Logo href="/" />
@@ -63,7 +63,7 @@ class Header extends Component {
                         timeout={200}
                         classNames="slide" >
                             <NavSearch className={focused?"focused":""}
-                            onFocus={handleInputFocus} 
+                            onFocus={()=>handleInputFocus(list)} 
                             onBlur={handleInputBlur}>
                             </NavSearch>
                         </CSSTransition>
@@ -101,8 +101,11 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        handleInputFocus(){
-            dispatch(actionCreators.getList())
+        handleInputFocus(list){
+            // if(list.size===0){
+            //     dispatch(actionCreators.getList());
+            // }    可以这么写：
+            (list.size===0) && dispatch(actionCreators.getList());
             dispatch(actionCreators.searchFocus());
         },
         handleInputBlur(){
