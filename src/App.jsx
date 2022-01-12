4d8183a7de34c776cb0d25aedc7e4c8e4cd3e521
way99 <745855589@qq.com>
2022-01-12 10:15:46 +0800

    feat add 首页组件的划分

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux';
+import { BrowserRouter,Route } from 'react-router-dom';
 import Header from './common/header';
+import Home from './pages/home';
+import Detail from './pages/detail';
 import { GlobalStyle } from './style';
 import { GlobalStyleIconFont } from './statics/iconfont/iconfont';
 import store from './store'
@@ -11,7 +14,15 @@ class App extends Component {
                 <GlobalStyle/>
                 <GlobalStyleIconFont/>
                 <Provider store={store}>
-                    <Header />
+                    <div>
+                        <Header />
+                        <BrowserRouter>
+                            <div>
+                                <Route path='/' exact component={Home}></Route>
+                                <Route path='/detail' exact component={Detail}></Route>
+                            </div>
+                        </BrowserRouter>
+                    </div>
                 </Provider>
             </div>
             
---
+++ src/pages/home/style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const HomeWrapper=styled.div`
+    overflow:hidden;
+    width:960px;
+    margin:0 auto;
+`
+export const HomeLeft=styled.div`
+    float:left;
+    width:625px;
+    margin-left:15px;
+    padding-top:30px;
+    .banner-img{
+        width:625px;
+        height:270px;
+    }
+`
+export const HomeRight=styled.div`
+    width:240px;
+    float:right;
+`
