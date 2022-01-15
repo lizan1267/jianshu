60315aa61d761d66e36b0e7682d8a81077373881
way99 <745855589@qq.com>
2022-01-15 17:28:12 +0800

    feat add 详情页异步获取数据

--- src/pages/detail/index.jsx
+++ src/pages/detail/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { DetailWrapper, Header, Content } from './style'
+import { actionCreators } from './store'
 
 class Detail extends Component {
     render() {
@@ -11,10 +12,18 @@ class Detail extends Component {
             </DetailWrapper>
         )
     }
+    componentDidMount(){
+        this.props.getDetail();
+    }
 }
 const mapState=(state)=>({
     title:state.getIn(["detail","title"]),
     content:state.getIn(["detail","content"])
 })
+const mapDispatch=(dispatch)=>({
+    getDetail(){
+        dispatch(actionCreators.getDetail())
+    }
+})
 
-export default connect(mapState,null)(Detail);
+export default connect(mapState,mapDispatch)(Detail);
