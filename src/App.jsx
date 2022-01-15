8704eb05d08789d9052eb86d062e37e44be198e4
way99 <745855589@qq.com>
2022-01-15 22:24:47 +0800

    feat add 异步组件及withRouter路由

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter,Route } from 'react-router-dom';
 import Header from './common/header';
 import Home from './pages/home';
-import Detail from './pages/detail';
+import Detail from './pages/detail/loadable';
 import { GlobalStyle } from './style';
 import { GlobalStyleIconFont } from './statics/iconfont/iconfont';
 import Login from './pages/login'
